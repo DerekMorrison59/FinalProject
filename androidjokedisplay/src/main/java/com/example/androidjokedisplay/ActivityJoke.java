755b6502d9e7d2109337b3074be929a6bae4446f
DerekMorrison59 <DerekMorrison59@gmail.com>
2016-04-22 15:02:27 -0600

    Code cleanup - some additional sanity checks, add comments and comment out logging statements

--- androidjokedisplay/src/main/java/com/example/androidjokedisplay/ActivityJoke.java
+++ androidjokedisplay/src/main/java/com/example/androidjokedisplay/ActivityJoke.java
@@ -1,5 +1,6 @@
 package com.example.androidjokedisplay;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,25 +8,33 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ActivityJoke extends AppCompatActivity {
-    private final String LOG_TAG = ActivityJoke.class.getSimpleName();
+    //private final String LOG_TAG = ActivityJoke.class.getSimpleName();
 
     public static final String JOKE_KEY = "JOKE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i(LOG_TAG, "onCreate");
+        //Log.i(LOG_TAG, "onCreate");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // set up the action bar to display the 'back' arrow
+        ActionBar actionBar = getSupportActionBar();
+        if (null != actionBar) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
+        // provide a default string to display
         String joke = "Nothing received from the library and that's no joke!";
 
+        // the intent should have an extras bundle that contains the joke string
         Bundle bun = getIntent().getExtras();
 
         if (null != bun) {
-            joke = bun.getString(JOKE_KEY);
+            if (bun.containsKey(JOKE_KEY)) {
+                joke = bun.getString(JOKE_KEY);
+            }
         }
 
         TextView jokeSpace = (TextView) findViewById(R.id.jokeTextView);
